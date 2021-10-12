e99b6edb7b84e00ff6bbb8142f1423bb092c71be
Adam <qingxianyang@gmail.com>
2021-10-12 22:54:29 +0800

    暂存部分代码

--- lib/class.Plan.php
+++ lib/class.Plan.php
@@ -14,6 +14,9 @@ class Plan extends Base{
     const FULL_DAY_DEFAULT_YES='Yes';
     const FULL_DAY_DEFAULT_NO='No';
 
+    const DISPLAY_IN_TABLE_YES='Yes';
+    const DISPLAY_IN_TABLE_NO='No';
+
     public function GetActivePlan(){
         $sql=sprintf(
             "select ID,Name from %s where Deleted=0 and status in ('%s','%s') order by ID desc;",
@@ -28,7 +31,14 @@ class Plan extends Base{
 
     public function List()
     {
-        $sql=sprintf("select * from %s where Deleted=0 and status not in ('%s','%s') order by ID desc;",static::$table,self::STATUS_ARCHIVED,self::STATUS_GIVE_UP);
+        $displayInTable=$this->get['DisplayInTable'] ?? self::DISPLAY_IN_TABLE_YES;
+        $sql=sprintf(
+            "select * from %s where Deleted=0 and status not in ('%s','%s') and Display_In_Table='%s' order by ID desc;",
+            static::$table,
+            self::STATUS_ARCHIVED,
+            self::STATUS_GIVE_UP,
+            $displayInTable
+        );
         $plans=$this->pdo->getRows($sql);
         $planItem=new PlanItem();
         foreach ($plans as &$plan){
@@ -43,6 +53,8 @@ class Plan extends Base{
         empty($this->post['Deleted']) && $this->post['Deleted']=0;
         empty($this->post['status']) && $this->post['status']=self::STATUS_PROCESSING;
         empty($this->post['UpdateFinishTime']) && $this->post['UpdateFinishTime']=self::UPDATE_FINISH_TIME_SHOW;
+        empty($this->post['Full_Day_Default']) && $this->post['Full_Day_Default']=self::FULL_DAY_DEFAULT_YES;
+        empty($this->post['Display_In_Table']) && $this->post['Display_In_Table']=self::DISPLAY_IN_TABLE_YES;
         return $this->handleSql($this->post,$this->post['ID'],'ID');
     }
 }
