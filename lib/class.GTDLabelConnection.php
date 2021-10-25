4ae1c60189d7bd4f96af9494cf38ebbb79923847
Adam <qingxianyang@gmail.com>
2021-10-25 22:54:55 +0800

    暂存部分代码

--- lib/class.GTDLabelConnection.php
+++ lib/class.GTDLabelConnection.php
@@ -3,7 +3,65 @@
 class GTDLabelConnection extends Base{
     public static $table="GTD_Label_Connection";
 
-    public function getLabels($GTD_ID=0,$Label_ID=0){
+    public function GetGTDLabel(){
+        $GTDID=$this->get['ID'] ?? 0;
+        if (empty($GTDID)){
+            return self::returnActionResult(
+                $this->get,
+                false,
+                "参数错误"
+            );
+        }
+        return self::returnActionResult(
+            [
+                'Connection'=>$this->getLabels($GTDID)
+            ]
+        );
+    }
+
+    public function UpdateConnection(){
+        $this->post=json_decode($this->post,1);
+        $GTDID=$this->post['GTD_ID'] ?? 0;
+        $LabelIDs=$this->post['Label_ID'] ?? [];
+        if (empty($GTDID) || empty($LabelIDs)){
+            return self::returnActionResult(
+                $this->post,
+                false,
+                "参数错误"
+            );
+        }
+        $labels=$this->getLabels($GTDID,0,'ID');
+        $insert=[];
+        foreach ($LabelIDs as $index=>$labelID){
+            if (!isset($labels[$labelID])){
+                // insert 部分
+                $insert[]=$labelID;
+                unset($labels[$index]);
+            }
+        }
+        $deleted=array_keys($labels);
+        if (!empty($insert)){
+            foreach ($insert as $labelId){
+                $this->handleSql([
+                    'GTD_ID'=>$GTDID,
+                    'Label_ID'=>$labelId
+            ],0);
+            }
+        }
+        if (!empty($deleted)){
+            $sql=sprintf("delete from %s where Label_ID in (%s)",static::$table,implode(",",$deleted));
+            $this->pdo->query($sql);
+        }
+        return self::returnActionResult(
+            [
+                'post'=>$this->post,
+                'insert'=>$insert,
+                'delete'=>$deleted
+            ]
+        );
+    }
+
+    public function getLabels($GTD_ID=0,$Label_ID=0,$keyName=''){
         if (empty($GTD_ID) && empty($Label_ID)){
             return false;
         }
@@ -13,6 +71,6 @@ class GTDLabelConnection extends Base{
             $where=sprintf("where Label_ID=%d",$Label_ID);
         }
         $sql=sprintf("select * from %s %s",static::$table,$where);
-        return $this->pdo->getRows($sql);
+        return $this->pdo->getRows($sql,$keyName);
     }
 }
