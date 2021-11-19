c3c14e81ac006db32e7de2d5834ed0a6cc51181c
yangqingxian <qingxianyang@gmail.com>
2021-11-18 19:32:50 -0800

    暂存所有代码

--- lib/class.OKRDecision.php
+++ lib/class.OKRDecision.php
@@ -6,4 +6,17 @@ class OKRDecision extends Base{
     const STATUS_PROCESSING='processing';
     const STATUS_FINISHED='finished';
     const STATUS_GIVE_UP='give_up';
+
+    public function getDecisions($ItemId,$statusMap){
+        if (!is_array($statusMap)){
+            $statusMap[]=$statusMap;
+        }
+        $status=[];
+        foreach ($statusMap as $statusItem){
+            $status[]=sprintf("'%s'",$statusItem);
+        }
+        $status=sprintf("(%s)",implode(",",$status));
+        $sql=sprintf("select * from %s where OKR_Item_ID=%d and status in %s",static::$table,$ItemId,$status);
+        return $this->pdo->getRows($sql);
+    }
 }
--- lib/class.OKRItem.php
+++ lib/class.OKRItem.php
@@ -13,4 +13,41 @@ class OKRItem extends Base{
 
     const TYPE_WEEK='week';
     const TYPE_MONTH='month';
+
+    public function getItems(){
+        $this->post=json_decode($this->post,1);
+        $OKR_ID=$this->post['OKR_ID'] ?? 0;
+        if ($OKR_ID<=0){
+            return self::returnActionResult(
+                $this->post,
+                false,
+                "Param Error"
+            );
+        }
+        if (empty($this->post['status'])){
+            $status=sprintf("('%s','%s')",self::STATUS_PROCESSING,self::STATUS_INIT);
+        }else{
+            $status=[];
+            foreach ($this->post['status'] as $statusItem){
+                $status[]=sprintf("'%s'",$statusItem);
+            }
+            $status=sprintf("(%s)",implode(",",$status));
+        }
+        $sql=sprintf(
+            "select * from %s where OKR_ID=%d and status in %s",
+            static::$table,
+            $OKR_ID,
+            $status
+        );
+        $OKR_Items=$this->pdo->getRows($sql);
+        $OKRDecision=new OKRDecision();
+        foreach ($OKR_Items as &$item){
+            $item['OKR_Decisions']=$OKRDecision->getDecisions($item['ID'],OKRDecision::STATUS_PROCESSING);
+        }
+        return self::returnActionResult(
+            [
+                'Items'=>$this->pdo->getRows($sql)
+            ]
+        );
+    }
 }
