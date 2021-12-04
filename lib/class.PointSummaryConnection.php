fb43a7024f4d1f2c0fede3fe031c89dbdad9794b
MiddleSunshine <qingxianyang@gmail.com>
2021-12-04 21:37:12 +0800

    保存部分文件

--- lib/class.PointSummaryConnection.php
+++ lib/class.PointSummaryConnection.php
@@ -5,5 +5,48 @@ class PointSummaryConnection extends Base{
 
     public function NewConnection(){
         $this->post=json_decode($this->post,1);
+        if(empty($this->post['SID']) || empty($this->post['PID'])){
+            return self::returnActionResult($this->post,false,"参数错误");
+        }
+        if($this->checkConnection($this->post['SID'],$this->post['PID'])){
+            return self::returnActionResult($this->post);
+        }
+    }
+
+    public function deleteAllPID($sid){
+        if(empty($sid)){
+            return false;
+        }
+        $sql=sprintf("delete from %s where SID=%d",self::$table,$sid);
+        return $this->pdo->query($sql);
+    }
+
+    public function deleteConnection($sid,$pid){
+        if(empty($sid) || empty($pid)){
+            return false;
+        }
+        $sql=sprintf("delete from %s where SID=%d and PID=%d;",self::$table,$sid,$pid);
+        $this->pdo->query($sql);
+        return !$this->checkConnection($sid,$pid);
+    }
+
+    public function checkConnection($sid,$pid){
+        if(empty($sid) || empty($pid)){
+            return false;
+        }
+        $sql=sprintf("select * from %s where SID=%d and PID=%d",self::$table,$sid,$pid);
+        return $this->pdo->getFirstRow($sql);
+    }
+
+    public function createNewConnection($sid,$pid){
+        if(empty($sid) || empty($pid)){
+            return false;
+        }
+        $data=[
+            'SID'=>$sid,
+            'PID'=>$pid
+        ];
+        $this->handleSql($data,0);
+        return true;
     }
 }
