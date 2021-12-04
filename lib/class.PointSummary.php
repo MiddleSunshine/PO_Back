c9106735fddcedb537be9bf2f54d478178b02a9f
qingxianyang <qingxianyang@gmail.com>
2021-12-04 22:49:07 +0800

    后台代码开发完成

--- lib/class.PointSummary.php
+++ lib/class.PointSummary.php
@@ -14,12 +14,19 @@ class PointSummary extends Base{
         }
         $this->post['AddTime']=date("Y-m-d H:i:s");
         $this->post['LastUpdateTime']=date("Y-m-d H:i:s");
+        if(!empty($this->post['file_content'])){
+            file_put_contents(SummaryFilePath.DIRECTORY_SEPARATOR.$this->post['Title'].".md",$this->post['file_content']);
+        }
         $this->handleSql($this->post,0);
+
         return self::returnActionResult();
     }
 
     public function preSave()
     {
+        if(!empty($this->post['file_content']) && !empty($this->post['Title'])){
+            file_put_contents(SummaryFilePath.DIRECTORY_SEPARATOR.$this->post['Title'].".md",$this->post['file_content']);
+        }
         $this->post['LastUpdateTime']=date("Y-m-d H:i:s");
     }
 
