12829164a64863c2498a96d48034d415a04f2b91
MiddleSunshine <qingxianyang@gmail.com>
2021-12-04 21:15:33 +0800

    常识vscode提交代码

--- lib/class.PointSummaryConnection.php
+++ lib/class.PointSummaryConnection.php
@@ -3,4 +3,7 @@
 class PointSummaryConnection extends Base{
     public static $table="PS_Connection";
 
+    public function NewConnection(){
+        $this->post=json_decode($this->post,1);
+    }
 }
