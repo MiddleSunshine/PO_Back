ca23355c62107d204b66208a021f28a2c56ea35b
qingxianyang <qingxianyang@gmail.com>
2021-12-04 20:40:05 +0800

    git push尝试

--- lib/class.PointTagConnection.php
+++ lib/class.PointTagConnection.php
@@ -2,4 +2,10 @@
 
 class PointTagConnection extends Base{
     public static $table="PS_Tag_Connection";
+
+    public function getSummaryTagConnection($summaryId){
+        $sql=sprintf("select * from %s where PID=%d;",static::$table,$summaryId);
+        $TagIds=$this->pdo->getRows($sql,'TID');
+        return array_keys($TagIds);
+    }
 }
