f9f7e8caccee12d97b4c3b7a8d9c8ccda915e047
Adam <qingxianyang@gmail.com>
2021-12-05 14:08:40 +0800

    细节修改

--- lib/class.PointSummary.php
+++ lib/class.PointSummary.php
@@ -3,6 +3,30 @@
 class PointSummary extends Base{
     public static $table="Point_Summary";
 
+    public function Detail()
+    {
+        $id=$this->get['ID'] ?? 0;
+        if(empty($id)){
+            return self::returnActionResult(
+                $this->get,
+                false,
+                "参数错误"
+            );
+        }
+        $sql=sprintf("select * from %s where ID=%d;",static::$table,$id);
+        $pointSummary=$this->pdo->getFirstRow($sql);
+        if(!empty($pointSummary['file'])){
+            $pointSummary['file_content']=file_get_contents(SummaryFilePath.DIRECTORY_SEPARATOR.$pointSummary['file'].".md");
+        }else{
+            $pointSummary['file_content']="";
+        }
+        return self::returnActionResult(
+            [
+                'PointSummary'=>$pointSummary
+            ]
+        );
+    }
+
     public function NewPointSummary(){
         $this->post=json_decode($this->post,1);
         if (empty($this->post['Title'])){
--- lib/class.PointTagConnection.php
+++ lib/class.PointTagConnection.php
@@ -5,26 +5,26 @@ class PointTagConnection extends Base{
 
     public function NewConnection(){
         $this->post=json_decode($this->post,true);
-        if(empty($this->post['PID']) || empty($this->post['TID'])){
+        if(empty($this->post['PS_ID']) || empty($this->post['TID'])){
             return self::returnActionResult($this->post,false,"参数错误");
         }
-        if($this->checkConnection($this->post['PID'],$this->post['TID'])){
+        if($this->checkConnection($this->post['PS_ID'],$this->post['TID'])){
             return self::returnActionResult(
                 $this->post
             );
         }
-        $this->createConnection($this->post['PID'],$this->post['TID']);
+        $this->createConnection($this->post['PS_ID'],$this->post['TID']);
         return self::returnActionResult();
     }
 
     public function DeleteConnection(){
         $this->post=json_decode($this->post,true);
-        if(empty($this->post['PID']) || empty($this->post['TID'])){
+        if(empty($this->post['PS_ID']) || empty($this->post['TID'])){
             return self::returnActionResult($this->post,false,"参数错误");
         }
-        $sql=sprintf("delete from %s where PID=%d and TID=%d;",static::$table,$this->post['PID'],$this->post['TID']);
+        $sql=sprintf("delete from %s where PS_ID=%d and TID=%d;",static::$table,$this->post['PS_ID'],$this->post['TID']);
         $this->pdo->query($sql);
-        if($this->checkConnection($this->post['PID'],$this->post['TID'])){
+        if($this->checkConnection($this->post['PS_ID'],$this->post['TID'])){
             return self::returnActionResult(
                 $this->post,
                 false,
@@ -35,28 +35,28 @@ class PointTagConnection extends Base{
     }
 
 
-    public function createConnection($PID,$TID){
-        if(empty($PID) || empty($TID)){
+    public function createConnection($PS_ID,$TID){
+        if(empty($PS_ID) || empty($TID)){
             return false;
         }
         $data=[
-            'PID'=>$PID,
+            'PS_ID'=>$PS_ID,
             'TID'=>$TID
         ];
         $this->handleSql($data,0);
         return true;
     }
 
-    public function checkConnection($PID,$TID){
-        if(empty($PID) || empty($TID)){
+    public function checkConnection($PS_ID,$TID){
+        if(empty($PS_ID) || empty($TID)){
             return false;
         }
-        $sql=sprintf("select * from %s where PID=%d and TID=%d",static::$table,$PID,$TID);
+        $sql=sprintf("select * from %s where PS_ID=%d and TID=%d",static::$table,$PS_ID,$TID);
         return $this->pdo->getFirstRow($sql);
     }
 
     public function getSummaryTagConnection($summaryId){
-        $sql=sprintf("select * from %s where PID=%d;",static::$table,$summaryId);
+        $sql=sprintf("select * from %s where PS_ID=%d;",static::$table,$summaryId);
         $TagIds=$this->pdo->getRows($sql,'TID');
         return array_keys($TagIds);
     }
