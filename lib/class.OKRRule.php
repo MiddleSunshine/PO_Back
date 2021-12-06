f9b19217da5e0c0dad9181bcef85f9287f3e91f0
yangqingxian <qingxianyang@gmail.com>
2021-12-06 01:24:26 -0800

    暂存所有代码

--- lib/class.OKRRule.php
+++ lib/class.OKRRule.php
@@ -2,4 +2,23 @@
 
 class OKRRule extends Base{
     public static $table="OKR_Rule";
+
+    const STATUS_ACTIVE='active';
+    const STATUS_INACTIVE='inactive';
+
+    public function NewRule(){
+        $this->post=json_decode($this->post,1);
+        if (empty($this->post['Rule'])){
+            return self::returnActionResult(
+                $this->post,
+                false,
+                "Wrong Param"
+            );
+        }
+        $this->post['Status']=self::STATUS_ACTIVE;
+        $this->post['AddTime']=date("Y-m-d H:i:s");
+        $this->post['Year']=date("Y");
+        $this->post['Month']=date("n");
+        return $this->handleSql($this->post,0);
+    }
 }
