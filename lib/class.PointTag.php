3533f1b59706e39c49307484869fad67148fa9a6
Adam <qingxianyang@gmail.com>
2021-12-06 11:43:09 +0800

    细节调整

--- lib/class.PointTag.php
+++ lib/class.PointTag.php
@@ -3,6 +3,14 @@
 class PointTag extends Base{
     public static $table="Point_Tag";
 
+    public function List()
+    {
+        $sql=sprintf("select * from %s where Deleted=0 order by ID desc;",static::$table);
+        return self::returnActionResult(
+            $this->pdo->getRows($sql)
+        );
+    }
+
     public function NewTag(){
         $this->post=json_decode($this->post,1);
         if(empty($this->post['Tag'])){
@@ -12,5 +20,7 @@ class PointTag extends Base{
                 "参数错误"
             );
         }
+        $this->handleSql($this->post,0);
+        return self::returnActionResult();
     }
 }
