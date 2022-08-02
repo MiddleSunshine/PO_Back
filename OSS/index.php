ee69d008ae707d4c249650096ad928c12862d381
Adam <qingxianyang@gmail.com>
2022-08-02 13:47:14 +0800

    上传遗漏文件

--- OSS/index.php
+++ OSS/index.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 require_once __DIR__.DIRECTORY_SEPARATOR."UploadFile.php";
+require_once __DIR__.DIRECTORY_SEPARATOR."SyncMysql.php";
 
 $method=$_GET['method'] ?? "";
 
@@ -27,6 +28,7 @@ switch ($method){
         }catch (Throwable $e){
             echo CommonReturn(false,"Throwable",[$e->getMessage(),$e->getFile(),$e->getLine()]);
         }
+        break;
     default:
         echo CommonReturn(false,"Method Not Exists");
 }
