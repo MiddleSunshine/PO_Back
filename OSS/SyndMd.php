40e8e26dd13cf169fdf2b1b361ad15adc8165a44
yangqingxian <qingxianyang@gmail.com>
2022-08-26 02:02:41 -0700

    每星期备份一次md文件夹下所有文件

---
+++ OSS/SyndMd.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "config.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "UploadFile.php";
+
+class SyndMd
+{
+    public function syncMd()
+    {
+        $files = self::getFiles(MD_FILE_INDEX);
+        $upload = new UploadFile();
+        foreach ($files as $file) {
+            $mdFiles = self::getFiles(MD_FILE_INDEX . $file);
+            if (empty($mdFiles)) {
+                continue;
+            }
+            foreach ($mdFiles as $mdFile) {
+                $upload->Upload(LONG_STORE_BUCKET, MD_FILE_INDEX . $file . DIRECTORY_SEPARATOR . $mdFile, "PO_Back_MD_BackUp".DIRECTORY_SEPARATOR.date("Y-m-d") . DIRECTORY_SEPARATOR . $file . DIRECTORY_SEPARATOR . $mdFile);
+            }
+        }
+    }
+
+    public static function getFiles($dir)
+    {
+        if (!is_dir($dir)) {
+            return [];
+        }
+        $files = scandir($dir);
+        unset($files[0]);
+        unset($files[1]);
+        return $files;
+    }
+}
--- Worker/worker.CronJob.php
+++ Worker/worker.CronJob.php
@@ -1,9 +1,15 @@
 <?php
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR."OSS".DIRECTORY_SEPARATOR."SyncMysql.php";
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR."OSS".DIRECTORY_SEPARATOR."SyndMd.php";
 
 class CronJob{
     public function mysqlDumper(){
         $syncMysql=new SyncMysql();
         $syncMysql->mysqldumper();
+    }
+
+    public function backUpMdFileContent(){
+        $syncMd=new SyndMd();
+        $syncMd->syncMd();
     }
 }
