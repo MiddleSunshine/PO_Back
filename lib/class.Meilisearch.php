0df707f3d8d98a108ec59832d768d06f594548de
yangqingxian <qingxianyang@gmail.com>
2022-09-19 02:49:03 -0700

    保存部分代码

---
+++ lib/class.Meilisearch.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Worker".DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
+use MeiliSearch\Client;
+
+class Meilisearch extends ElasticSearch {
+    /**
+     * @var Client
+     */
+    public $client;
+    public function __construct()
+    {
+        $this->client=new Client(ES_SERVER);
+    }
+
+    public function NewDatabase($databseName)
+    {
+        return true;
+    }
+
+    public function StoreDocument($index, $ID, $storeData)
+    {
+        $this->client->index($index)->addDocuments($storeData);
+    }
+
+    public function DeleteDocument($index, $ID)
+    {
+        // todo
+    }
+
+    public function SearchMultipleFileds($index, $search, $source = 'ID')
+    {
+        $searchResult=$this->client->index($index)->search($search);
+
+    }
+
+    public function SearchOneField($index, $field, $keyword, $source = 'ID')
+    {
+        return false;
+    }
+}
