93f6890ae57b379d094ff19b34e49f03b464d45a
yangqingxian <qingxianyang@gmail.com>
2022-10-20 02:47:55 -0700

    注释多余代码

--- OSS/SyndMd.php
+++ OSS/SyndMd.php
@@ -6,17 +6,18 @@ class SyndMd
 {
     public function syncMd()
     {
-        $files = self::getFiles(MD_FILE_INDEX);
-        $upload = new UploadFile();
-        foreach ($files as $file) {
-            $mdFiles = self::getFiles(MD_FILE_INDEX . $file);
-            if (empty($mdFiles)) {
-                continue;
-            }
-            foreach ($mdFiles as $mdFile) {
-                $upload->Upload(LONG_STORE_BUCKET, MD_FILE_INDEX . $file . DIRECTORY_SEPARATOR . $mdFile, "PO_Back_MD_BackUp".DIRECTORY_SEPARATOR.date("Y-m-d") . DIRECTORY_SEPARATOR . $file . DIRECTORY_SEPARATOR . $mdFile);
-            }
-        }
+        // todo 这里调整为使用zip打包的效果
+//        $files = self::getFiles(MD_FILE_INDEX);
+//        $upload = new UploadFile();
+//        foreach ($files as $file) {
+//            $mdFiles = self::getFiles(MD_FILE_INDEX . $file);
+//            if (empty($mdFiles)) {
+//                continue;
+//            }
+//            foreach ($mdFiles as $mdFile) {
+//                $upload->Upload(LONG_STORE_BUCKET, MD_FILE_INDEX . $file . DIRECTORY_SEPARATOR . $mdFile, "PO_Back_MD_BackUp".DIRECTORY_SEPARATOR.date("Y-m-d") . DIRECTORY_SEPARATOR . $file . DIRECTORY_SEPARATOR . $mdFile);
+//            }
+//        }
     }
 
     public static function getFiles($dir)
