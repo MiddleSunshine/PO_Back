4427a6b081a97404caae56b7b739a685dd2e66f8
qingxianyang <qingxianyang@gmail.com>
2022-12-03 19:23:37 +0800

    后端接口大体框架完成

--- lib/class.Explain.php
+++ lib/class.Explain.php
@@ -3,6 +3,17 @@
 class Explain extends Base{
     public static $table='Scare_Explain';
 
+    public function NewExplain(){
+        $this->post=json_decode($this->post,1);
+        if(empty($this->post['Explain'])){
+            return self::returnActionResult($this->post,false,'Please Input The Explain');
+        }
+        $this->post['AddTime']=date("Y-m-d H:i:s");
+        $this->post['LastUpdateTime']=date("Y-m-d H:i:s");
+        $this->handleSql($this->post,0);
+        return self::returnActionResult();
+    }
+
     public function getExplains($scareId){
         if (empty($scareId)){
             return [];
