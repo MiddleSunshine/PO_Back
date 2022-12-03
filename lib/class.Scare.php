2b2b41f192902bc10a798ab1e476cfccc4d976d8
qingxianyang <qingxianyang@gmail.com>
2022-12-03 19:17:09 +0800

    暂存部分代码

--- lib/class.Scare.php
+++ lib/class.Scare.php
@@ -15,10 +15,28 @@ class Scare extends Base{
     }
 
     public function ScareList(){
-
+        $status=$this->get['Status'] ?? self::STATUS_ACTIVE;
+        $sql=sprintf("select * from %s where Status='%s' order by ID desc;",self::$table,$status);
+        $scares=$this->pdo->getRows($sql);
+        $scareExplain=new Explain();
+        foreach ($scares as $scare){
+            $scare['Explains']=$scareExplain->getExplains($scare['ID']);
+        }
+        return self::returnActionResult([
+            'ScareList'=>$scares
+        ]);
     }
 
     public function NewScare(){
-
+        $this->post=json_decode($this->post,1);
+        if (empty($this->post['Scare'])){
+            return self::returnActionResult($this->post,false,"Please input the scare");
+        }
+        empty($this->post['Status']) && $this->post['Status']=self::STATUS_ACTIVE;
+        empty($this->post['PID']) && $this->post['PID']=0;
+        $this->post['AddTime']=date("Y-m-d H:i:s");
+        $this->post['LastUpdateTime']=date("Y-m-d H:i:s");
+        $this->handleSql($this->post,0);
+        return self::returnActionResult();
     }
 }
